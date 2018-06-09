766ec531ee172cf785bd59667c312b1cc30aaeca
Phil Martin <phil.martin1983@gmail.com>
2018-06-09 23:36:24 +0100

    Initial Commit

---
+++ basicobjectdetectionTests/basicobjectdetectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  basicobjectdetectionTests.swift
+//  basicobjectdetectionTests
+//
+//  Created by Phil Martin on 09/06/2018.
+//  Copyright © 2018 Phil Martin. All rights reserved.
+//
+
+import XCTest
+@testable import basicobjectdetection
+
+class basicobjectdetectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
